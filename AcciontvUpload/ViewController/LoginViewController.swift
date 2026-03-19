75e47f67f5c51ff3e42777cd0e1ab9e06dda78fd
Daniel Carrera <carreraprogrammer@gmail.com>
2026-03-20 03:25:28 +0530

    fix: update project version and marketing version to 16 and 0.1.7 respectively; add loginButton outlet in LoginViewController; adjust scheme order hints in xcschememanagement.plist

--- AcciontvUpload/ViewController/LoginViewController.swift
+++ AcciontvUpload/ViewController/LoginViewController.swift
@@ -11,17 +11,14 @@ import UIKit
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var versionLbl: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
     var userHasToken = false
     var token: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-      
-        
-        
-        
-        versionLbl.text = "Version: \(Bundle.main.releaseVersionNumber!) (\(Bundle.main.buildVersionNumber!))"
+        SettingsBundleHelper.checkAndExecuteSettings()
+        applyEnvironmentStyle()
         
         print("TOKEN")
         print(UserDefaults.standard.string(forKey: Identifier.Token))
@@ -37,6 +34,11 @@ class LoginViewController: UIViewController {
             UserDefaults.standard.removeObject(forKey: "app_token")
         }
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        applyEnvironmentStyle()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -46,6 +48,40 @@ class LoginViewController: UIViewController {
         performSegue(withIdentifier: Identifier.WebView, sender: self)
     }
     
+}
+
+// MARK: - Environment Style
+
+extension LoginViewController {
+    private func applyEnvironmentStyle() {
+        let env = SettingsBundleHelper.getEnvironment()
+        let version = Bundle.main.releaseVersionNumber ?? ""
+        let build = Bundle.main.buildVersionNumber ?? ""
+
+        if env == "Production" {
+            versionLbl.text = "VERSION \(version) (\(build))"
+            return
+        }
+
+        versionLbl.text = "VERSION \(version) (\(build)) - \(env)"
+
+        switch env {
+        case "Testing":
+            view.backgroundColor = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1)
+            loginButton.backgroundColor = UIColor(red: 24/255, green: 106/255, blue: 59/255, alpha: 1)
+        case "Staging":
+            view.backgroundColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1)
+            loginButton.backgroundColor = UIColor(red: 23/255, green: 82/255, blue: 121/255, alpha: 1)
+        case "Development":
+            view.backgroundColor = UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)
+            loginButton.backgroundColor = UIColor(red: 175/255, green: 96/255, blue: 26/255, alpha: 1)
+        case "Local":
+            view.backgroundColor = UIColor(red: 142/255, green: 68/255, blue: 173/255, alpha: 1)
+            loginButton.backgroundColor = UIColor(red: 91/255, green: 44/255, blue: 111/255, alpha: 1)
+        default:
+            break
+        }
+    }
 }
 
 // MARK: - Navigation
@@ -83,5 +119,3 @@ extension Bundle {
         return infoDictionary?["CFBundleVersion"] as? String
     }
 }
-
-
